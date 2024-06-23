66de7044c162a49eee6baf5cdc4eae9fc66df8f8
vellimir <kkazancov@gmail.com>
2024-06-23 19:12:06 +0400

    models, page coach

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Coach(models.Model):
+    img_coach = models.ImageField(
+        upload_to="coach_images/",
+        blank=False, null=False,
+        verbose_name="Изображения тренера"
+    )
+    name = models.CharField(
+        max_length=20, blank=False, null=False,
+        verbose_name="Имя тренера"
+    )
+    surname = models.CharField(
+        max_length=20, blank=False, null=False,
+        verbose_name="Фамилия тренера"
+    )
+    patronymic = models.CharField(
+        max_length=20, blank=False, null=False,
+        verbose_name="Отчество тренера"
+    )
+    directions = models.CharField(
+        max_length=50, blank=False, null=False,
+        verbose_name="Направления"
+    )
+    phone = models.CharField(
+        max_length=20, blank=False, null=False,
+        verbose_name="Номер телефона"
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "тренера"
+        verbose_name_plural = "Тренерский состав"
+
