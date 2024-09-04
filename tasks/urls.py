2eec18c650f56b95ec7b2ed3e393ddf6f7fe3611
vellimir <kkazancov@gmail.com>
2024-09-04 17:59:51 +0400

    delete tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('task-details/<int:task_id>/', views.task_details, name='task_details'),
     path('task-confirm/<int:task_id>/', views.confirm_execution_task, name='task_confirm'),
     path('change-task-status/<int:task_id>/<int:new_status>/', views.change_task_status, name='change_task_status'),
+    path('delete-task/<int:task_id>/', views.delete_task, name='delete_task'),
 
 ]
