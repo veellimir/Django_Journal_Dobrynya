36b4dfe7340bbd5a7e5033f42478fcc06da5b378
vellimir <kkazancov@gmail.com>
2024-12-17 16:04:00 +0300

    fixed admin panel for profile, calendar

--- app/app_schedules/apps.py
+++ app/app_schedules/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AppSchedulesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.app_schedules'
-    verbose_name = 'Календарь тренировок'
+    verbose_name = 'Календарь'
--- app/mainapp/base_mixins.py
+++ app/mainapp/base_mixins.py
@@ -1,7 +1,9 @@
 class StrMixin:
     def __str__(self) -> str:
-        if hasattr(self, "name"):
+        if hasattr(self, "competition_name") and self.competition_name:
+            return f"{self.competition_name}"
+        elif hasattr(self, "name") and self.name:
             return str(self.name)
-        elif hasattr(self, "title"):
+        elif hasattr(self, "title") and self.title:
             return self.title
         return super().__str__()
